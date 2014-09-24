a66bc4d8d96324b8a2b9d609b4954c3404ebde0e
ruthmcanavan <ruthmcanavan@gmail.com>
2014-09-24 18:34:37 -0400

    Sorting quiz
    Created framework

---
+++ Project 2/src/Project2.java
@@ -0,0 +1,70 @@
+/**
+ * A quiz to determine which Hogwarts house you are in
+ */
+	import java.util.*;
+
+/**
+ * @author ruthcanavan
+ *
+ */
+public class Project2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner console = new Scanner(System.in); 
+		
+		int bravery = console.nextInt(); 
+		int wisdom = console.nextInt();
+		int loyalty = console.nextInt();
+		int cunning = console.nextInt();
+
+	}
+
+		public static char question1(); {
+			System.out.println("If you could create a new potion that would give you one thing, what would you choose?");
+			System.out.println("A. Love");
+			System.out.println("B. Wisdom");
+			System.out.println("C. Glory");
+			System.out.println("D. Power");
+			
+			
+		}
+	
+
+		public static char question2(); {
+			System.out.println("");
+			System.out.println("A. ");
+			System.out.println("B. ");
+			System.out.println("C. ");
+			System.out.println("D. ");
+		}
+		
+		public static char question3(); {
+			System.out.println("How would you like to be remembered in history?");
+			System.out.println("A. Being wise");
+			System.out.println("B. Being good");
+			System.out.println("C. Being great");
+			System.out.println("D. Being bold");
+		}
+		
+		public static char question4(); {
+			System.out.println("");
+			System.out.println("A. ");
+			System.out.println("B. ");
+			System.out.println("C. ");
+			System.out.println("D. ");
+		}
+		
+		public static char question5(); {
+			System.out.println("");
+			System.out.println("A. ");
+			System.out.println("B. ");
+			System.out.println("C. ");
+			System.out.println("D. ");
+		}
+		
+		
+}
