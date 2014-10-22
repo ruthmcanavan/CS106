43c31eea5dd0299ba7b122edc74bced3092545b1
ruthmcanavan <ruthmcanavan@gmail.com>
2014-10-22 18:59:01 -0400

    project 5
    some stuff

---
+++ Project 5/src/Analysis.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author ruthcanavan
+ *
+ */
+	public class Analysis { 
+		fileReader = new Scanner(new File("src/corkRain.txt"));
+		while (fileReader.hasNext()) {
+		}
+}
+	//Skip over the month
+	input.next();
+	
+	//Find how many months have over 100mm of rainfall
+	int numMonths = 0; {
+	if (rain >= 100){
+		numMonths++;
+			
+		}
+	}
+}
---
+++ Project 5/src/Project5.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+import java.util.*;
+import java.io.FileNotFoundException;
+/**
+ * @author ruthcanavan
+ *Program to analyze the months when County Cork, Ireland has the highest average rainfall 
+ */
+public class Project5 {
+
+	/**
+	 * @param args
+	 * @see http://www.cork.climatemps.com/cork-climate-graph.gif 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+
+	}
+
+}
