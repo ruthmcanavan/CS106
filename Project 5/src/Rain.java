45ae6b651636264e8dc78e08e5d700d2941ece32
ruthmcanavan <ruthmcanavan@gmail.com>
2014-10-29 18:41:27 -0400

    Edits
    stuff has been changed

---
+++ Project 5/src/Rain.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author ruthcanavan
+ * a class to create the necessary variables
+ * @param year the year
+ * @param month month of the year
+ * @param mm millimeters of rainfall
+ */
+public class Rain {
+	//list year, month and millimeters of rain variables
+	public String year;
+	public String month;
+	public int mm;
+	
+	public Rain(String year, String month, int mm) {
+		//set variables
+		this.year = year;
+		this.month = month;
+		this.mm = mm;
+
+}
+}
