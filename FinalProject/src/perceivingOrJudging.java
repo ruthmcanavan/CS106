f4d631ba53ae818b5136ac4ae022d783bd665fb4
ruthmcanavan <ruthmcanavan@gmail.com>
2014-12-03 11:29:51 -0500

    Changed
    Read in files

--- FinalProject/src/perceivingOrJudging.java
+++ FinalProject/src/perceivingOrJudging.java
@@ -1,104 +1,115 @@
+import java.io.*;
+import java.util.*;
 /**
  
 /**
  * @author ruthcanavan
  *Determine if the subject is P or J
  */
-public class perceivingOrJudging {
+public class perceivingOrJudging 
+	throws FileNotFoundException {
+	//read in file with answers
+	fileReader = new Scanner(new File("src/answers.txt"));
+	while (fileReader.hasNext()) {
+	
+	
 	//declare variables
 	int perceiving = 0;
 	int judging = 0; {
 
-	//1. I like to have things decided 
+	//35. I like to have things decided 
 		if (anwser = true){
 			judging++;
 		} else {
 			judging--;
 		}
 	
-	//2. I like to approach work as play or mix work and play 
+	//36. I like to approach work as play or mix work and play 
 		if (anwser = true){
 			perceiving++;
 		} else {
 			perceiving--;
 		}
 	
-	//3. I appear to be task oriented 
+	//37. I appear to be task oriented 
 		if (anwser = true){
 			judging++;
 		} else {
 			judging--;
 		}
 	
-	//4. I am stimulated by an approaching deadline 
+	//38. I am stimulated by an approaching deadline 
 		if (anwser = true){
 			perceiving++;
 		} else {
 			perceiving--;
 		}
 	
-	//5. I work in bursts of energy 
+	//39. I work in bursts of energy 
 		if (anwser = true){
 			perceiving++;
 		} else {
 			perceiving--;
 		}
 	
-	//6. I plan work to avoid rushing just before a deadline 
+	//40. I plan work to avoid rushing just before a deadline 
 		if (anwser = true){
 			judging++;
 		} else {
 			judging--;
 		}
 	
-	//7. Sometimes I focus so much on the goal that I miss new information 
+	//41. Sometimes I focus so much on the goal that I miss new information 
 		if (anwser = true){
 			judging++;
 		} else {
 			judging--;
 		}
 	
-	//8. I like to make lists of things to do 
+	//42. I like to make lists of things to do 
 		if (anwser = true){
 			judging++;
 		} else {
 			judging--;
 		}
 	
-	//9. I like to stay open to respond to whatever happens 
+	//43. I like to stay open to respond to whatever happens 
 		if (anwser = true){
 			perceiving++;
 		} else {
 			perceiving--;
 		}
 	
-	//10. I like to get my work done before playing 
+	//44. I like to get my work done before playing 
 		if (anwser = true){
 			judging++;
 		} else {
 			judging--;
 		}
 	
-	//11. I appear to be loose and casual. I like to keep plans to a minimum 
+	//45. I appear to be loose and casual. I like to keep plans to a minimum 
 		if (anwser = true){
 			perceiving++;
 		} else {
 			perceiving--;
 		}
 	
-	//12. Sometimes I stay open to new information so long I miss making decisions when they are needed 
+	//46. Sometimes I stay open to new information so long I miss making decisions when they are needed 
 		if (anwser = true){
 			perceiving++;
 		} else {
 			perceiving--;
 		}
 		
+		//Determine whether subject is P or J and print results
 		if(perceiving > judging) {
-			return perceiving;
+			System.out.print("   P");
 		}else if(judging > perceiving) {
-			return judging;
-		}else
-			return null;
+			System.out.print("   J");
+		}else  //Print X if subject's P score equals their J score
+			System.out.print("   X");
 		
 }
 }
+}
+
